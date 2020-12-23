757563998df3e3bd96360dda65e20cdec5712d0e
Touré A. Karim <trabdlkarim@programmer.net>
2020-12-24 02:57:58 +0300

    Update custom.js

--- docs/_static/js/custom.js
+++ docs/_static/js/custom.js
@@ -1,1 +1,10 @@
 // Custom JS
+// Simple JavaScript for setting footer anchors target attritube
+
+var footerAnchors = document.getElementsByTagName("FOOTER")[0].getElementsByTagName("A"); 
+
+footerAnchors.forEach(setAnchorTarget);
+
+function setAnchorTarget(anchor,index,array){
+    anchor.setAttribute("target","_blank");
+}

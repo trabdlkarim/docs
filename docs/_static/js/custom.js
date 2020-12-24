ef7477b14f6debc50972a35af79a094f8c8b6395
Touré A. Karim <trabdlkarim@programmer.net>
2020-12-24 04:33:57 +0300

    Update custom.js

--- docs/_static/js/custom.js
+++ docs/_static/js/custom.js
@@ -2,24 +2,17 @@
 // Simple JavaScript for setting footer anchors target attritube
 
 window.addEventListener("load", function(){
-
 var children = document.getElementsByTagName("FOOTER")[0].children; 
-const  footerAnchors = [];
 
 if(children.length > 0){
-  for(let i=0; i<children.length; i++){
+    for(let i=0; i<children.length; i++){
       if(children[i].nodeName == "A"){
-          footerAnchors.push(children[i]);
+          setAnchorTarget(children[i]);
       }
+    }
   }
-}
-
-if(footerAnchors.length>0){
-    footerAnchors.forEach(setAnchorTarget);
-}
-  
 });
 
-function setAnchorTarget(anchor,index,array){
+function setAnchorTarget(anchor){
     anchor.setAttribute("target","_blank");
 }

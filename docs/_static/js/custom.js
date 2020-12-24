0f868b91982dbbb6c853bbc5314a57b8dbfad288
Touré A. Karim <trabdlkarim@programmer.net>
2020-12-24 03:48:18 +0300

    Update custom.js

--- docs/_static/js/custom.js
+++ docs/_static/js/custom.js
@@ -8,7 +8,7 @@ if(children.length > 0){
   var i;
   for(i=0;i<children.length;i++){
       if(children[i].nodeName == "A"){
-          footeranchors.append(children[i]);
+          footerAnchors.append(children[i]);
       }
   }
 }
